219d4421895b9bba2489501b939d16c9b2bd3dd0
Mr2RAEC <90472851+Mr2RAEC@users.noreply.github.com>
2022-08-24 21:24:32 -0500

    Segundo commit

--- Principal.java
+++ Principal.java
@@ -5,18 +5,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int vida,rp;
-		boolean fl = false;
-		vida=0;
+		int var;
 		Scanner sc = new Scanner(System.in);
-		do {
-		vida ++;
-		System.out.println("Vida:"+ vida + "\t¿Cual es el numero secreto?");
-		}while(vida<3 && !fl);
-		if (fl == true)
-			System.out.println("Adivinaste");
-		else
-			System.out.println("ups");
-	}
-
+		System.out.println("Ingrese numero");
+		var = sc.nextInt();
+		System.out.println("numero: "+ var);
+}
 }
