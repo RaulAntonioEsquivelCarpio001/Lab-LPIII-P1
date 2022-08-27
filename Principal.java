5b3a0eb7fad794cb9bf1087c269233f7ec0610e9
Mr2RAEC <90472851+Mr2RAEC@users.noreply.github.com>
2022-08-27 13:17:17 -0500

    Cambios en código clase principal. Recibe número y si acertamos nos dice el rpemio qeu ganamos. Rama 1

--- Principal.java
+++ Principal.java
@@ -5,8 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int num;
-		int Nvidas=3;
+		int numer;
+		int Nvid=3;
 		boolean cond=false;
 		String prem1="Yakuza Song of Life";
 		Scanner sc = new Scanner(System.in);
@@ -14,16 +14,16 @@ public class Principal {
 		
 		do {
 			System.out.println("Ingrese numero");
-			num = sc.nextInt();
-			if (num==var1) {
+			numer = sc.nextInt();
+			if (numer==var1) {
 				System.out.println("Premio: "+prem1);
 				break;
 			}
 			if (cond==false) {
 				System.out.println("Mala suerte");
-				Nvidas --;
+				Nvid --;
 			}
-		}while(Nvidas!=0);
+		}while(Nvid!=0);
 		
 }
 }
